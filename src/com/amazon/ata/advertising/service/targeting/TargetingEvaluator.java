24b3bcc89a1ecf9fe85a1081b1a307946ff2b82c
Santiago Coria <santiago_coria@hotmail.com>
2024-11-07 13:45:28 -0300

    Mastery Task2

--- src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
+++ src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
@@ -3,23 +3,32 @@ package com.amazon.ata.advertising.service.targeting;
 import com.amazon.ata.advertising.service.model.RequestContext;
 import com.amazon.ata.advertising.service.targeting.predicate.TargetingPredicate;
 import com.amazon.ata.advertising.service.targeting.predicate.TargetingPredicateResult;
+import org.apache.logging.log4j.core.tools.picocli.CommandLine;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 /**
  * Evaluates TargetingPredicates for a given RequestContext.
  */
 public class TargetingEvaluator {
     public static final boolean IMPLEMENTED_STREAMS = true;
-    public static final boolean IMPLEMENTED_CONCURRENCY = false;
+    public static final boolean IMPLEMENTED_CONCURRENCY = true;
     private final RequestContext requestContext;
+    private final ExecutorService executorService;
 
     /**
      * Creates an evaluator for targeting predicates.
      * @param requestContext Context that can be used to evaluate the predicates.
      */
     public TargetingEvaluator(RequestContext requestContext) {
+
         this.requestContext = requestContext;
+        this.executorService = Executors.newFixedThreadPool(10);
     }
 
     /**
@@ -30,10 +39,27 @@ public class TargetingEvaluator {
      */
     public TargetingPredicateResult evaluate(TargetingGroup targetingGroup) {
         List<TargetingPredicate> targetingPredicates = targetingGroup.getTargetingPredicates();
-        boolean allTruePredicates = targetingPredicates.stream()
-                .map(predicate -> predicate.evaluate(requestContext))
-                .allMatch(TargetingPredicateResult::isTrue);
+
+        List<Future<TargetingPredicateResult>> futures = targetingPredicates.stream()
+                .map(predicate -> executorService.submit(() -> predicate.evaluate(requestContext)))
+                .collect(Collectors.toList());
+
+        boolean allTruePredicates = futures.stream().allMatch(future -> {
+            try {
+                return future.get().isTrue();  // Retrieve and check result
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt(); // Restore interrupted status
+                return false; // Consider interrupted tasks as failed
+            } catch (ExecutionException e) {
+                System.err.println("Error evaluating predicate: " + e.getCause());
+                return false; // Consider failed tasks as false
+            }
+        });
 
         return allTruePredicates ? TargetingPredicateResult.TRUE : TargetingPredicateResult.FALSE;
+    }
+
+    public void shutdown() {
+        executorService.shutdown();
     }
 }
